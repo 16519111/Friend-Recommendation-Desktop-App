3a06406b0db361e34ac938614e81488789d9332f
16519111 <gregdimasbaskara@gmail.com>
2021-03-12 15:05:17 +0700

    Read .txt successful

--- Tubes2_App/Tubes2_App/MainWindow.xaml.cs
+++ Tubes2_App/Tubes2_App/MainWindow.xaml.cs
@@ -23,31 +23,32 @@ namespace Tubes2_App
     {
         public MainWindow()
         {
-            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
-            //create a viewer object 
-            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
-            //create a graph object 
-            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
-            //create the graph content 
-            graph.AddEdge("A", "B");
-            graph.AddEdge("B", "C");
-            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
-            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
-            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
-            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
-            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
-            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
-            //bind the graph to the viewer 
-            viewer.Graph = graph;
-            //associate the viewer with the form 
-            form.SuspendLayout();
-            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            form.Controls.Add(viewer);
-            form.ResumeLayout();
-            //show the form 
-            form.ShowDialog();
+            InitializeComponent();
 
-            //InitializeComponent();
+            // ------------------------ GRAPH TESTER --------------------------- //
+            //System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            ////create a viewer object 
+            //Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            ////create a graph object 
+            //Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            ////create the graph content 
+            //graph.AddEdge("A", "B");
+            //graph.AddEdge("B", "C");
+            //graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            //graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            //graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            //Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            //c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+            ////bind the graph to the viewer 
+            //viewer.Graph = graph;
+            ////associate the viewer with the form 
+            //form.SuspendLayout();
+            //viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            //form.Controls.Add(viewer);
+            //form.ResumeLayout();
+            ////show the form 
+            //form.ShowDialog();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -60,13 +61,19 @@ namespace Tubes2_App
 
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Browse_File_Button(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.DefaultExt = ".txt"; // Required file extension 
             fileDialog.Filter = "Text documents (.txt)|*.txt"; // Optional file extensions
+            fileDialog.Multiselect = false;
 
-            fileDialog.ShowDialog();
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string sFileName = fileDialog.FileName;
+                string[] lines = System.IO.File.ReadAllLines(@sFileName);
+                System.Windows.Forms.MessageBox.Show(lines[0], "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
     }
 }
