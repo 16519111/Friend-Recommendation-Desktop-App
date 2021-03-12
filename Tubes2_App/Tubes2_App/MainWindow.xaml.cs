d85f7e45a15f467a40791d44c314a3a8d160c05c
16519111 <gregdimasbaskara@gmail.com>
2021-03-12 17:52:34 +0700

    Read Input and Visualize Graph

--- Tubes2_App/Tubes2_App/MainWindow.xaml.cs
+++ Tubes2_App/Tubes2_App/MainWindow.xaml.cs
@@ -24,31 +24,6 @@ namespace Tubes2_App
         public MainWindow()
         {
             InitializeComponent();
-
-            // ------------------------ GRAPH TESTER --------------------------- //
-            //System.Windows.Forms.Form form = new System.Windows.Forms.Form();
-            ////create a viewer object 
-            //Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
-            ////create a graph object 
-            //Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
-            ////create the graph content 
-            //graph.AddEdge("A", "B");
-            //graph.AddEdge("B", "C");
-            //graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
-            //graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
-            //graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
-            //Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
-            //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
-            //c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
-            ////bind the graph to the viewer 
-            //viewer.Graph = graph;
-            ////associate the viewer with the form 
-            //form.SuspendLayout();
-            //viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            //form.Controls.Add(viewer);
-            //form.ResumeLayout();
-            ////show the form 
-            //form.ShowDialog();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -70,9 +45,43 @@ namespace Tubes2_App
 
             if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
+                // Get directory file .txt yang dipilih
                 string sFileName = fileDialog.FileName;
+
+                // Baca line per line kemudian dimasukkan ke array of string lines
                 string[] lines = System.IO.File.ReadAllLines(@sFileName);
+
+                // Memunculkan dialog untuk testing
                 System.Windows.Forms.MessageBox.Show(lines[0], "Error Title", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                // ------------------------ GRAPH TESTER --------------------------- //
+                System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+                //create a viewer object 
+                Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+                //create a graph object 
+                Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+                //create the graph content
+                for (int i=1;i<lines.Length;i++)
+                {
+                    graph.AddEdge(lines[i][0].ToString(), lines[i][2].ToString());
+                }
+                //graph.AddEdge("A", "B");
+                //graph.AddEdge("B", "C");
+                //graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+                //graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+                //graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+                //Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+                //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+                //c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+                //bind the graph to the viewer 
+                viewer.Graph = graph;
+                //associate the viewer with the form 
+                form.SuspendLayout();
+                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                form.Controls.Add(viewer);
+                form.ResumeLayout();
+                //show the form 
+                form.ShowDialog();
             }
         }
     }
