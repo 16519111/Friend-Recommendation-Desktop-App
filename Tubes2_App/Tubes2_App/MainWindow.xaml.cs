02b683c97752b215253d27ad5676a9f84024b587
16519111 <gregdimasbaskara@gmail.com>
2021-03-13 06:34:48 +0700

    Trying Bitmap (Masih fail)

--- Tubes2_App/Tubes2_App/MainWindow.xaml.cs
+++ Tubes2_App/Tubes2_App/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Controls;
@@ -74,15 +75,24 @@ namespace Tubes2_App
                 //c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
                 //c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
                 //bind the graph to the viewer 
-                viewer.Graph = graph;
-                //associate the viewer with the form 
-                form.SuspendLayout();
-                viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-                form.Controls.Add(viewer);
-                form.ResumeLayout();
-                //show the form 
-                form.ShowDialog();
+                //viewer.Graph = graph;
+                ////associate the viewer with the form 
+                //form.SuspendLayout();
+                //viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+                //this.graphBox.
+                //form.Controls.Add(viewer);
+                //form.ResumeLayout();
+                ////show the form 
+                //form.ShowDialog();
+                Microsoft.Msagl.GraphViewerGdi.GraphRenderer renderer = new Microsoft.Msagl.GraphViewerGdi.GraphRenderer(graph);
+                renderer.CalculateLayout();
+                int width = 50;
+                Bitmap bitmap = new Bitmap(width, (int)(graph.Height *
+                (width / graph.Width)));
+                renderer.Render(bitmap);
+                bitmap.Save("test.png");
             }
+            this.graphBox.Visibility = Visibility.Visible;
         }
     }
 }
